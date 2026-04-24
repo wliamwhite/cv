3d4b59f5c8af161cc63bf9b8a0802f60daed66f5
Liam White <Wliamwhite@gmail.com>
2026-04-23 21:37:32 -0500

    fexed css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					assetFileNames: '_astro/[hash][extname]',
+					chunkFileNames: '_astro/[hash].js',
+					entryFileNames: '_astro/[hash].js',
+				},
+			},
+		},
+	},
+});

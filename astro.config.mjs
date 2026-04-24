ddc98bb8dfcf6d48219eeb24b5ff2a6a523746cd
Liam White <Wliamwhite@gmail.com>
2026-04-23 21:48:30 -0500

    style overhaul v2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		assets: 'assets',
+	},
 	vite: {
 		build: {
 			rollupOptions: {
 				output: {
-					assetFileNames: '_astro/[hash][extname]',
-					chunkFileNames: '_astro/[hash].js',
-					entryFileNames: '_astro/[hash].js',
+					assetFileNames: 'assets/[hash][extname]',
+					chunkFileNames: 'assets/[hash].js',
+					entryFileNames: 'assets/[hash].js',
 				},
 			},
 		},
